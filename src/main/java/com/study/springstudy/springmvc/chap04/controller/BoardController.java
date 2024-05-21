2be7d51c986abf8b69fde0b39a96b832902a7b73
chanhee7 <chan9505@naver.com>
2024-05-21 16:16:27 +0900

    mybatis, Service, Mapper 활용한 코드개선

--- src/main/java/com/study/springstudy/springmvc/chap04/controller/BoardController.java
+++ src/main/java/com/study/springstudy/springmvc/chap04/controller/BoardController.java
@@ -5,7 +5,6 @@ import com.study.springstudy.springmvc.chap04.dto.BoardWriteRequestDto;
 import com.study.springstudy.springmvc.chap04.dto.BoardListResponseDto;
 import com.study.springstudy.springmvc.chap04.entity.Board;
 import com.study.springstudy.springmvc.chap04.repository.BoardRepository;
-import com.study.springstudy.springmvc.chap04.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,35 +20,30 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BoardController {
 
-    private final BoardService service;
-//    private final BoardRepository boardRepository;
+    private final BoardRepository boardRepository;
 
     // 1. 목록 조회 요청 (/board/list : GET)
     @GetMapping("/list")
     public String list(Model model) {
 
         // 1. 데이터베이스로 부터 게시글 목록 조회
-//        List<Board> boardList = boardRepository.findAll();
-//
-//        // 2. 클라이언트에 데이터를 보내기전에 렌더링에 필요한
-//        //    데이터만 추출하기
-////        boardList.stream()
-////                .map(board -> new BoardListResponseDto())
-////                .collect(Collectors.toList());
-//
-//        List<BoardListResponseDto> bList = new ArrayList<>();
-//
-//        for (Board b : boardList) {
-//            BoardListResponseDto dto = new BoardListResponseDto(b);
-//            bList.add(dto);
-//        }
-//        boardRepository.findAll();
+        List<Board> boardList = boardRepository.findAll();
 
-        // 서비스에게 조회 요청 위임
-        List<BoardListResponseDto> all = service.findAll();
+        // 2. 클라이언트에 데이터를 보내기전에 렌더링에 필요한
+        //    데이터만 추출하기
+//        boardList.stream()
+//                .map(board -> new BoardListResponseDto())
+//                .collect(Collectors.toList());
+
+        List<BoardListResponseDto> bList = new ArrayList<>();
+
+        for (Board b : boardList) {
+            BoardListResponseDto dto = new BoardListResponseDto(b);
+            bList.add(dto);
+        }
 
         // 3. JSP 파일에 해당 목록데이터를 보냄
-        model.addAttribute("bList", all);
+        model.addAttribute("bList", bList);
 
         return "board/list";
     }
@@ -70,12 +64,12 @@ public class BoardController {
         // 1. 브라우저가 전달한 게시글 내용 읽기
 
         // 2. 해당 게시글을 데이터베이스에 저장하기 위해  엔터티 클래스로 변환
-//        Board b = dto.toEntity();
+//        Board b = new Board(dto);
+        Board b = dto.toEntity();
 
         // 3. 데이터베이스 저장 명령
-//        service.save(b);
+        boardRepository.save(b);
 
-        service.save(dto);
         return "redirect:/board/list";
     }
 
@@ -85,7 +79,7 @@ public class BoardController {
     @GetMapping("/delete")
     public String delete(int bno) {
 
-        service.delete(bno);
+        boardRepository.delete(bno);
         return "redirect:/board/list";
     }
 
@@ -98,12 +92,11 @@ public class BoardController {
         System.out.println("bno = " + bno);
 
         // 2. 데이터베이스로부터 해당 글번호 데이터 조회하기
-//        Board board = boardRepository.findOne(bno);
-//        if (board != null) boardRepository.viewCount(bno);
-        BoardDetailResponseDto dto = service.findOne(bno);
+        Board board = boardRepository.findOne(bno);
+        if (board != null) boardRepository.viewCount(bno);
 
         // 3. JSP 파일에 조회한 데이터 보내기
-        model.addAttribute("b", dto);
+        model.addAttribute("b", new BoardDetailResponseDto(board));
 
         return "board/detail";
     }
