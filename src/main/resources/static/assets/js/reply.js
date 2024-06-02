2bb603c89265f7e0c787a992abf26d534b180605
chan <chan9505@naver.com>
2024-06-02 21:53:11 +0900

    댓글 수정 백엔드

--- src/main/resources/static/assets/js/reply.js
+++ src/main/resources/static/assets/js/reply.js
@@ -6,22 +6,6 @@ import { modifyReplyClickEvent } from "./modifyReply.js";
 // ======== 전역 변수 ========
 export const BASE_URL = "http://localhost:8383/api/v1/replies";
 
-
-// ======== 함수 정의 ========
-function post(BASE_URL, payload) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', BASE_URL);
-
-  xhr.setRequestHeader('content-type', 'application/json');
-  xhr.send(JSON.stringify(payload));
-
-  xhr.onload = e => {
-    const replies = JSON.parse(xhr.response);
-    console.log(replies);
-  };
-}
-
-
 // ======== 실행 코드 ========
 
 // 댓글 목록 서버에서 불러오기
