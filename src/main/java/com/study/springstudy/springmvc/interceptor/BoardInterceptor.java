58199e62e588189589d5153b57be23c67db5bbb8
chanhee7 <chan9505@naver.com>
2024-06-03 17:36:08 +0900

    # 내가 쓴글만 삭제 처리 인터셉터 설정

--- src/main/java/com/study/springstudy/springmvc/interceptor/BoardInterceptor.java
+++ src/main/java/com/study/springstudy/springmvc/interceptor/BoardInterceptor.java
@@ -1,35 +1,68 @@
 package com.study.springstudy.springmvc.interceptor;
 
+import com.study.springstudy.springmvc.chap04.entity.Board;
+import com.study.springstudy.springmvc.chap04.mapper.BoardMapper;
 import com.study.springstudy.springmvc.util.LoginUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class BoardInterceptor implements HandlerInterceptor {
 
-    // preHandle을 구현하여
+    private final BoardMapper boardMapper;
+
+    // preHandle 을 구현하여
     // 로그인을 안한 회원은 글쓰기, 글수정, 글삭제 요청을 거부할 것!
     // 거부하고 로그인 페이지로 리다이렉션할 것!
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        if (!LoginUtil.isLoggedIn(request.getSession())) {
+        HttpSession session = request.getSession();
 
-            String redirectUri = request.getRequestURI();
+        // 요청 URL
+        String redirectUri = request.getRequestURI();
 
+        if (!LoginUtil.isLoggedIn(session)) {
             log.info("origin: {}", redirectUri);
             response.sendRedirect("/members/sign-in?message=login-required&redirect=" + redirectUri);
             return false;
         }
 
         // 삭제요청이 들어오면 서버에서 한번더 관리자인지? 자기가쓴글인지 체크
+        // 관리자 인가?
+        if (LoginUtil.isAdmin(session)) {
+            return true;
+        }
 
+        // 삭제 요청인지?
+        if (redirectUri.equals("/board/delete")) {
+            System.out.println("redirectUri = " + redirectUri);
+            // 내가 쓴 글이 아닌지?
+            // 현재 삭제하려는 글의 글쓴이 계정명과
+            // -> DB 에서 조회해보면 됨 -> 글번호가 필요함
+            int bno = Integer.parseInt(request.getParameter("bno"));
+            Board board = boardMapper.findOne(bno);
+            String boardAccount = board.getAccount();
+
+            // 현재 로그인한 회원의 계정명을 구해서
+            String loggedInUserAccount = LoginUtil.getLoggedInUserAccount(session);
+
+            // 대조해보는 작업이 필요함
+            if (!LoginUtil.isMine(boardAccount, loggedInUserAccount)) {
+                response.sendRedirect("/access-deny?message=authorization");
+                return false;
+            }
+        }
 
         return true;
     }
+
 }
