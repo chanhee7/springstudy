f33b2169fded38933cd5a819c7c89e4085debe57
chan <chan9505@naver.com>
2024-06-06 16:34:38 +0900

    # 무한스크롤 로딩버그 개선

--- src/main/resources/static/assets/js/reply.js
+++ src/main/resources/static/assets/js/reply.js
@@ -1,4 +1,4 @@
-import { fetchInfScrollReplies, setupInfiniteScroll } from "./getReply.js";
+import { initInfScroll } from "./getReply.js";
 import { fetchReplyPost } from "./postReply.js";
 import { removeReplyClickEvent } from "./deleteReply.js";
 import { modifyReplyClickEvent } from "./modifyReply.js";
@@ -9,8 +9,10 @@ export const BASE_URL = "http://localhost:8383/api/v1/replies";
 // ======== 실행 코드 ========
 
 // 댓글 목록 서버에서 불러오기
+initInfScroll();
+
 // fetchReplies();
-fetchInfScrollReplies(); // 일단 1페이지 데이터 그려놓기
+// fetchInfScrollReplies(); // 일단 1페이지 데이터 그려놓기
 // setupInfiniteScroll(); // 무한 스크롤 이벤트 등록
 
 // 댓글 작성 이벤트 등록
